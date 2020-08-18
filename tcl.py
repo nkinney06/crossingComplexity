efa1a1a0334a1bbf67218b980299486430749f5c
nkinney <nkinney06@gmail.com>
2020-08-18 23:55:02 +0000

    README updated

--- tcl.py
+++ tcl.py
@@ -7,6 +7,7 @@ import sys
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import re
+import subprocess
 
 zoom = .08
 color = "Fragment"
@@ -29,19 +30,26 @@ outfile.write("render Tachyon structure.dat\n")
 outfile.write("quit\n")
 outfile.close()
 
-os.popen("vmd -dispdev text -e vmd.tcl\n");
-os.popen("tachyon structure.dat -o curve.bmp -format BMP -res 1050 1050\n");
+some_command = "vmd -dispdev text -e vmd.tcl"
+p = subprocess.Popen(some_command, stdout=subprocess.PIPE, shell=True)
+(output, err) = p.communicate()  
+p_status = p.wait()
+
+some_command = "tachyon structure.dat -o curve.bmp -format BMP -res 1050 1050"
+p = subprocess.Popen(some_command, stdout=subprocess.PIPE, shell=True)
+(output, err) = p.communicate()  
+p_status = p.wait()
 
 ############################################################################################################################
 ## main programming section
 ############################################################################################################################
-	
+
 # read the structure.dat file
 lines = []
 with open("structure.dat") as file_in:
     for line in file_in:
         lines.append(line)
-		
+
 # read the points of one of the space filling curves
 colors = [ lines[x+5] for x in range(len(lines)) if "Sphere" in lines[x] ]
 colors = list(set([ re.search(r'Color (.*) Tex',x).group(1) for x in colors ]))
@@ -55,20 +63,23 @@ hull = ConvexHull(pts)
 
 outfile = open("territory.dat", "w")
 for line in lines:
-	if "End_Scene" not in line:
-		outfile.write(line.rstrip() + "\n")
+        if "End_Scene" not in line:
+                outfile.write(line.rstrip() + "\n")
 
 for s in hull.simplices:
-	outfile.write('\nTRI\n')
-	outfile.write('  V0 {} {} {}\n'.format(pts[s[0],0],pts[s[0],1],pts[s[0],2]))
-	outfile.write('  V1 {} {} {}\n'.format(pts[s[1],0],pts[s[1],1],pts[s[1],2]))
-	outfile.write('  V2 {} {} {}\n'.format(pts[s[2],0],pts[s[2],1],pts[s[2],2]))
-	outfile.write('  TEXTURE\n')
-	outfile.write('    AMBIENT 0.1 DIFFUSE 0.85 SPECULAR 0 OPACITY .7\n')
-	outfile.write('    COLOR 0.0 0.0 0.5\n')
-	outfile.write('    TEXFUNC 0\n')
+        outfile.write('\nTRI\n')
+        outfile.write('  V0 {} {} {}\n'.format(pts[s[0],0],pts[s[0],1],pts[s[0],2]))
+        outfile.write('  V1 {} {} {}\n'.format(pts[s[1],0],pts[s[1],1],pts[s[1],2]))
+        outfile.write('  V2 {} {} {}\n'.format(pts[s[2],0],pts[s[2],1],pts[s[2],2]))
+        outfile.write('  TEXTURE\n')
+        outfile.write('    AMBIENT 0.1 DIFFUSE 0.85 SPECULAR 0 OPACITY .7\n')
+        outfile.write('    COLOR 0.0 0.0 0.5\n')
+        outfile.write('    TEXFUNC 0\n')
 outfile.write("\nEnd_Scene\n")
 outfile.close()
 
-os.popen("tachyon territory.dat -o chull.bmp -format BMP -res 1050 1050\n");
+some_command = "tachyon territory.dat -o chull.bmp -format BMP -res 1050 1050"
+p = subprocess.Popen(some_command, stdout=subprocess.PIPE, shell=True)
+(output, err) = p.communicate()  
+p_status = p.wait()
 
